af509d87873610de2f7fdcf279526b8bfb0eaafe
respectAnthony <fxmaleke@yahoo.com>
2022-09-19 13:34:01 +0200

    Add project files.

---
+++ Form1.Designer.vb
@@ -0,0 +1,264 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.btnCreate = New System.Windows.Forms.Button()
+        Me.btnDisplay = New System.Windows.Forms.Button()
+        Me.txtFileName = New System.Windows.Forms.TextBox()
+        Me.txtStudentNo = New System.Windows.Forms.TextBox()
+        Me.txtTest = New System.Windows.Forms.TextBox()
+        Me.txtGrpAss = New System.Windows.Forms.TextBox()
+        Me.txtIndvAss = New System.Windows.Forms.TextBox()
+        Me.txtSurname = New System.Windows.Forms.TextBox()
+        Me.txtName = New System.Windows.Forms.TextBox()
+        Me.btnAdd = New System.Windows.Forms.Button()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.Label4 = New System.Windows.Forms.Label()
+        Me.Label5 = New System.Windows.Forms.Label()
+        Me.Label6 = New System.Windows.Forms.Label()
+        Me.Label7 = New System.Windows.Forms.Label()
+        Me.DataGridView1 = New System.Windows.Forms.DataGridView()
+        Me.Label8 = New System.Windows.Forms.Label()
+        CType(Me.DataGridView1, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'btnCreate
+        '
+        Me.btnCreate.Location = New System.Drawing.Point(338, 58)
+        Me.btnCreate.Name = "btnCreate"
+        Me.btnCreate.Size = New System.Drawing.Size(141, 32)
+        Me.btnCreate.TabIndex = 0
+        Me.btnCreate.Text = "Create"
+        Me.btnCreate.UseVisualStyleBackColor = True
+        '
+        'btnDisplay
+        '
+        Me.btnDisplay.Location = New System.Drawing.Point(533, 12)
+        Me.btnDisplay.Name = "btnDisplay"
+        Me.btnDisplay.Size = New System.Drawing.Size(664, 36)
+        Me.btnDisplay.TabIndex = 1
+        Me.btnDisplay.Text = "Display"
+        Me.btnDisplay.UseVisualStyleBackColor = True
+        '
+        'txtFileName
+        '
+        Me.txtFileName.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle
+        Me.txtFileName.Location = New System.Drawing.Point(142, 61)
+        Me.txtFileName.Name = "txtFileName"
+        Me.txtFileName.Size = New System.Drawing.Size(172, 26)
+        Me.txtFileName.TabIndex = 2
+        '
+        'txtStudentNo
+        '
+        Me.txtStudentNo.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle
+        Me.txtStudentNo.Location = New System.Drawing.Point(142, 115)
+        Me.txtStudentNo.Name = "txtStudentNo"
+        Me.txtStudentNo.Size = New System.Drawing.Size(337, 26)
+        Me.txtStudentNo.TabIndex = 3
+        '
+        'txtTest
+        '
+        Me.txtTest.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle
+        Me.txtTest.Location = New System.Drawing.Point(142, 358)
+        Me.txtTest.Name = "txtTest"
+        Me.txtTest.Size = New System.Drawing.Size(337, 26)
+        Me.txtTest.TabIndex = 4
+        '
+        'txtGrpAss
+        '
+        Me.txtGrpAss.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle
+        Me.txtGrpAss.Location = New System.Drawing.Point(142, 306)
+        Me.txtGrpAss.Name = "txtGrpAss"
+        Me.txtGrpAss.Size = New System.Drawing.Size(337, 26)
+        Me.txtGrpAss.TabIndex = 5
+        '
+        'txtIndvAss
+        '
+        Me.txtIndvAss.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle
+        Me.txtIndvAss.Location = New System.Drawing.Point(142, 254)
+        Me.txtIndvAss.Name = "txtIndvAss"
+        Me.txtIndvAss.Size = New System.Drawing.Size(337, 26)
+        Me.txtIndvAss.TabIndex = 6
+        '
+        'txtSurname
+        '
+        Me.txtSurname.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle
+        Me.txtSurname.Location = New System.Drawing.Point(142, 202)
+        Me.txtSurname.Name = "txtSurname"
+        Me.txtSurname.Size = New System.Drawing.Size(337, 26)
+        Me.txtSurname.TabIndex = 7
+        '
+        'txtName
+        '
+        Me.txtName.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle
+        Me.txtName.Location = New System.Drawing.Point(142, 165)
+        Me.txtName.Name = "txtName"
+        Me.txtName.Size = New System.Drawing.Size(337, 26)
+        Me.txtName.TabIndex = 8
+        '
+        'btnAdd
+        '
+        Me.btnAdd.Location = New System.Drawing.Point(142, 411)
+        Me.btnAdd.Name = "btnAdd"
+        Me.btnAdd.Size = New System.Drawing.Size(337, 29)
+        Me.btnAdd.TabIndex = 9
+        Me.btnAdd.Text = "Add To File"
+        Me.btnAdd.UseVisualStyleBackColor = True
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(56, 64)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(80, 20)
+        Me.Label1.TabIndex = 10
+        Me.Label1.Text = "File Name"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(42, 118)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(94, 20)
+        Me.Label2.TabIndex = 11
+        Me.Label2.Text = "Student No."
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Location = New System.Drawing.Point(85, 165)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(51, 20)
+        Me.Label3.TabIndex = 12
+        Me.Label3.Text = "Name"
+        '
+        'Label4
+        '
+        Me.Label4.AutoSize = True
+        Me.Label4.Location = New System.Drawing.Point(62, 205)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(74, 20)
+        Me.Label4.TabIndex = 13
+        Me.Label4.Text = "Surname"
+        '
+        'Label5
+        '
+        Me.Label5.AutoSize = True
+        Me.Label5.Location = New System.Drawing.Point(9, 257)
+        Me.Label5.Name = "Label5"
+        Me.Label5.Size = New System.Drawing.Size(127, 20)
+        Me.Label5.TabIndex = 14
+        Me.Label5.Text = "Indv Assignment"
+        '
+        'Label6
+        '
+        Me.Label6.AutoSize = True
+        Me.Label6.Location = New System.Drawing.Point(12, 306)
+        Me.Label6.Name = "Label6"
+        Me.Label6.Size = New System.Drawing.Size(124, 20)
+        Me.Label6.TabIndex = 15
+        Me.Label6.Text = "Grp Assignment"
+        '
+        'Label7
+        '
+        Me.Label7.AutoSize = True
+        Me.Label7.Location = New System.Drawing.Point(96, 361)
+        Me.Label7.Name = "Label7"
+        Me.Label7.Size = New System.Drawing.Size(40, 20)
+        Me.Label7.TabIndex = 16
+        Me.Label7.Text = "Test"
+        '
+        'DataGridView1
+        '
+        Me.DataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize
+        Me.DataGridView1.Location = New System.Drawing.Point(533, 61)
+        Me.DataGridView1.Name = "DataGridView1"
+        Me.DataGridView1.RowHeadersWidth = 62
+        Me.DataGridView1.RowTemplate.Height = 28
+        Me.DataGridView1.Size = New System.Drawing.Size(664, 371)
+        Me.DataGridView1.TabIndex = 17
+        '
+        'Label8
+        '
+        Me.Label8.AutoSize = True
+        Me.Label8.Font = New System.Drawing.Font("Mistral", 10.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label8.Location = New System.Drawing.Point(41, 639)
+        Me.Label8.Name = "Label8"
+        Me.Label8.Size = New System.Drawing.Size(341, 23)
+        Me.Label8.TabIndex = 18
+        Me.Label8.Text = "Designed and coded by Hlompho Maleke 202105507"
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(9.0!, 20.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(1209, 463)
+        Me.Controls.Add(Me.Label8)
+        Me.Controls.Add(Me.DataGridView1)
+        Me.Controls.Add(Me.Label7)
+        Me.Controls.Add(Me.Label6)
+        Me.Controls.Add(Me.Label5)
+        Me.Controls.Add(Me.Label4)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.btnAdd)
+        Me.Controls.Add(Me.txtName)
+        Me.Controls.Add(Me.txtSurname)
+        Me.Controls.Add(Me.txtIndvAss)
+        Me.Controls.Add(Me.txtGrpAss)
+        Me.Controls.Add(Me.txtTest)
+        Me.Controls.Add(Me.txtStudentNo)
+        Me.Controls.Add(Me.txtFileName)
+        Me.Controls.Add(Me.btnDisplay)
+        Me.Controls.Add(Me.btnCreate)
+        Me.Name = "Form1"
+        Me.Text = "Student Grades Management Application"
+        CType(Me.DataGridView1, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents btnCreate As Button
+    Friend WithEvents btnDisplay As Button
+    Friend WithEvents txtFileName As TextBox
+    Friend WithEvents txtStudentNo As TextBox
+    Friend WithEvents txtTest As TextBox
+    Friend WithEvents txtGrpAss As TextBox
+    Friend WithEvents txtIndvAss As TextBox
+    Friend WithEvents txtSurname As TextBox
+    Friend WithEvents txtName As TextBox
+    Friend WithEvents btnAdd As Button
+    Friend WithEvents Label1 As Label
+    Friend WithEvents Label2 As Label
+    Friend WithEvents Label3 As Label
+    Friend WithEvents Label4 As Label
+    Friend WithEvents Label5 As Label
+    Friend WithEvents Label6 As Label
+    Friend WithEvents Label7 As Label
+    Friend WithEvents DataGridView1 As DataGridView
+    Friend WithEvents Label8 As Label
+End Class
---
+++ Form1.vb
@@ -0,0 +1,82 @@
+﻿Imports System.IO 'This is required to create the file path
+Imports System.Text 'This is required to update the text in the file
+
+Public Class Form1
+
+    Dim table As New DataTable()
+
+    Private Sub btnCreate_Click(sender As Object, e As EventArgs) Handles btnCreate.Click
+
+        Dim newFile As String = txtFileName.Text & ".txt" 'ensures that the fil created is a .txt file
+        Dim fs As FileStream = File.Create(newFile)
+        fs.Close()
+
+    End Sub
+
+    Private Sub btnAdd_Click(sender As Object, e As EventArgs) Handles btnAdd.Click
+
+        If txtStudentNo.Text = "" Or txtName.Text = "" Or txtSurname.Text = "" Or txtIndvAss.Text = "" Or txtGrpAss.Text = "" Or txtTest.Text = "" Then
+            Dim message As String = "An input field was left open! Do you wish to continue?"
+            MsgBox(message, MessageBoxIcon.Warning, "Warning") 'confirms if the users wants to contine with missing inputs;
+        Else
+            Dim text As New StringBuilder
+            text.AppendLine(txtStudentNo.Text & "," & txtName.Text & "," & txtSurname.Text & "," & txtIndvAss.Text & "," & txtGrpAss.Text & "," & txtTest.Text)
+            Dim filepath As String = txtFileName.Text & ".txt" 'sets the file path as the same path in the file name text box;
+            File.AppendAllText(filepath, text.ToString()) 'updates the text within the file & converts all data type to stings
+
+            txtStudentNo.Text = "" 'sets the initial text within the text box to empty;
+            txtName.Text = ""
+            txtSurname.Text = ""
+            txtIndvAss.Text = ""
+            txtGrpAss.Text = ""
+            txtTest.Text = ""
+        End If
+
+    End Sub
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+
+        table.Columns.Add("Student No.", Type.GetType("System.String")) 'sets the table contents
+        table.Columns.Add("First name", Type.GetType("System.String"))
+        table.Columns.Add("Surname", Type.GetType("System.String"))
+        table.Columns.Add("Indv Assign", Type.GetType("System.String"))
+        table.Columns.Add("Grp Assign", Type.GetType("System.String"))
+        table.Columns.Add("Test", Type.GetType("System.String"))
+
+        DataGridView1.DataSource = table
+
+    End Sub
+
+    Private Sub btnDisplay_Click(sender As Object, e As EventArgs) Handles btnDisplay.Click
+
+        Dim lines() As String
+        Dim vals() As String
+        Dim path As String = txtFileName.Text & ".txt" 'created a variable to hold the file name(path)
+
+        If txtFileName.Text = "" Then
+            MsgBox("Please enter the file name you would like to display!", MessageBoxIcon.Warning, "Warning")
+        Else
+            lines = File.ReadAllLines(path) 'making a template string doesn't work for files
+
+            For i As Integer = 0 To lines.Length - 1 Step +1 'for loop to iterate through each line and word
+
+                vals = lines(i).ToString().Split(",") 'remove each instance of "," after each word
+                Dim row(vals.Length - 1) As String
+
+                For j As Integer = 0 To vals.Length - 1 Step +1
+
+                    row(j) = vals(j).Trim()
+
+                Next j
+
+                table.Rows.Add(row)
+
+            Next i
+        End If
+
+
+
+    End Sub
+
+    'Designed and coded by Hlompho Maleke 202105507
+End Class
